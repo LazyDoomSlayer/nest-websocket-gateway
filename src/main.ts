7897e96986bea1638d10cd90e9e9e6773d31f4ea
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-02-15 14:24:20 +0100

    feat: add interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new IoAdapter(app));
-  // app.enableCors({
-  //   origin: '*',
-  //   methods: ['GET', 'POST'],
-  //   credentials: true,
-  // });
-  //
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  app.enableCors();
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
