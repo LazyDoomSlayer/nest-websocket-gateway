1a1dc21615748f1f025c035e0cb22db6b8990bf0
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-02-17 21:08:09 +0100

    bugfix: fix imports of services

--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
-
 import { WebsocketGateway } from './websocket-gateway';
+import { WebsocketValidationService } from './services/websocket-validation.service';
+import { WebsocketAuthService } from './services/websocket-auth.service';
 
 @Module({
-  providers: [WebsocketGateway],
+  providers: [
+    WebsocketGateway,
+    WebsocketAuthService,
+    WebsocketValidationService,
+  ],
+  exports: [WebsocketValidationService, WebsocketAuthService],
 })
 export class WebsocketModule {}
